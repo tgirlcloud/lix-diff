548e54365e083aff84d67eb3cca0447ffbf87f1e
Jalil David Salamé Messina <jalil.salame@gmail.com>
2025-06-15 00:15:25 +0200

    feat: switch to clap
    As far as I can tell, `argh` doesn't support multicall binaries, I could
    try to hack my own support, or we can swit to clap.
    
    The size difference isn't too much:
    
    | build                             | size (release) |
    |-----------------------------------|---------------:|
    | `argh`                            | `513KB`        |
    | `clap` (default-features = false) | `628KB`        |
    | `clap` (default-features = true)  | `731KB`        |

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, path::PathBuf};
 
-use argh::FromArgs;
+use clap::Parser;
 use color_eyre::Result;
 use diff::PackageListDiff;
 use nu_ansi_term::{Color, Style};
@@ -12,24 +12,23 @@ mod versioning;
 
 use self::parser::DiffRoot;
 
-#[derive(FromArgs, PartialEq, Debug)]
+#[derive(Parser, PartialEq, Debug)]
 /// List the package differences between two `NixOS` generations
 struct Args {
     /// the path to the lix bin directory
-    #[argh(option, short = 'l')]
+    #[arg(short, long)]
     lix_bin: Option<PathBuf>,
 
     /// the generation we are switching from
-    #[argh(positional)]
     before: PathBuf,
 
     /// the generation we are switching to
-    #[argh(positional, default = "PathBuf::from(\"/run/current-system/\")")]
+    #[arg(default_value = "/run/current-system/")]
     after: PathBuf,
 }
 
 fn main() -> Result<()> {
-    let args: Args = argh::from_env();
+    let args = Args::parse();
     let before = args.before;
     let after = args.after;
     let lix_bin = args.lix_bin;
